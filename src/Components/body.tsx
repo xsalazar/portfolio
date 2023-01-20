a3b3f80a32913afe8cf76614be71e5ee44e3a218
Xavier Salazar <x.salazar1729@gmail.com>
2023-01-19 19:53:33 -0800

    Modify image list, add modal, adjust type, and favicon

--- src/Components/body.tsx
+++ src/Components/body.tsx
@@ -1,13 +1,67 @@
-import { Container, ImageList, ImageListItem, Typography } from "@mui/material";
-import Image from "mui-image";
+import {
+  Box,
+  Container,
+  ImageList,
+  ImageListItem,
+  Modal,
+  Typography,
+} from "@mui/material";
 import React from "react";
+import { v4 as uuidv4 } from "uuid";
 
-export default class Body extends React.Component {
+interface PortfolioProps {}
+
+interface PortfolioState {
+  imageURLs: Array<string>;
+  isModalOpen: boolean;
+  selectedImage: string;
+}
+
+export default class Body extends React.Component<
+  PortfolioProps,
+  PortfolioState
+> {
+  constructor(props: PortfolioProps) {
+    super(props);
+
+    this.state = {
+      imageURLs: [],
+      isModalOpen: false,
+      selectedImage: "",
+    };
+
+    this.openModal = this.openModal.bind(this);
+    this.closeModal = this.closeModal.bind(this);
+  }
+
+  componentDidMount(): void {
+    this.setState({
+      imageURLs: [
+        `https://upload.wikimedia.org/wikipedia/commons/1/13/Tunnel_View%2C_Yosemite_Valley%2C_Yosemite_NP_-_Diliff.jpg`,
+        `https://upload.wikimedia.org/wikipedia/commons/1/13/Tunnel_View%2C_Yosemite_Valley%2C_Yosemite_NP_-_Diliff.jpg`,
+        `https://upload.wikimedia.org/wikipedia/commons/1/13/Tunnel_View%2C_Yosemite_Valley%2C_Yosemite_NP_-_Diliff.jpg`,
+        `https://upload.wikimedia.org/wikipedia/commons/1/13/Tunnel_View%2C_Yosemite_Valley%2C_Yosemite_NP_-_Diliff.jpg`,
+        `https://upload.wikimedia.org/wikipedia/commons/1/13/Tunnel_View%2C_Yosemite_Valley%2C_Yosemite_NP_-_Diliff.jpg`,
+        `https://upload.wikimedia.org/wikipedia/commons/1/13/Tunnel_View%2C_Yosemite_Valley%2C_Yosemite_NP_-_Diliff.jpg`,
+        `https://upload.wikimedia.org/wikipedia/commons/1/13/Tunnel_View%2C_Yosemite_Valley%2C_Yosemite_NP_-_Diliff.jpg`,
+        `https://upload.wikimedia.org/wikipedia/commons/1/13/Tunnel_View%2C_Yosemite_Valley%2C_Yosemite_NP_-_Diliff.jpg`,
+        `https://upload.wikimedia.org/wikipedia/commons/1/13/Tunnel_View%2C_Yosemite_Valley%2C_Yosemite_NP_-_Diliff.jpg`,
+        `https://upload.wikimedia.org/wikipedia/commons/1/13/Tunnel_View%2C_Yosemite_Valley%2C_Yosemite_NP_-_Diliff.jpg`,
+        `https://upload.wikimedia.org/wikipedia/commons/1/13/Tunnel_View%2C_Yosemite_Valley%2C_Yosemite_NP_-_Diliff.jpg`,
+        `https://upload.wikimedia.org/wikipedia/commons/1/13/Tunnel_View%2C_Yosemite_Valley%2C_Yosemite_NP_-_Diliff.jpg`,
+        `https://upload.wikimedia.org/wikipedia/commons/1/13/Tunnel_View%2C_Yosemite_Valley%2C_Yosemite_NP_-_Diliff.jpg`,
+        `https://upload.wikimedia.org/wikipedia/commons/1/13/Tunnel_View%2C_Yosemite_Valley%2C_Yosemite_NP_-_Diliff.jpg`,
+      ],
+    });
+  }
+
   render() {
+    const { imageURLs, isModalOpen, selectedImage } = this.state;
+
     return (
       <div style={{ height: "calc(100vh - 200px)" }}>
         <Container
-          maxWidth="lg"
+          maxWidth="md"
           sx={{
             alignItems: "center",
             display: "flex",
@@ -16,39 +70,62 @@ export default class Body extends React.Component {
             height: "100%",
           }}
         >
-          <Typography variant="h2" sx={{ marginRight: "auto" }}>
+          <Typography variant="h3" sx={{ marginRight: "auto" }}>
             Xavier Salazar
           </Typography>
           <Typography variant="caption" sx={{ marginRight: "auto" }}>
-            Digital and film portfolio
+            Digital and film photography
           </Typography>
-          <ImageList cols={1} gap={32} sx={{ height: "100%" }}>
-            <ImageListItem>
-              <Image
-                src={`https://upload.wikimedia.org/wikipedia/commons/1/13/Tunnel_View%2C_Yosemite_Valley%2C_Yosemite_NP_-_Diliff.jpg`}
-              />
-            </ImageListItem>
-
-            <ImageListItem>
-              <Image
-                src={`https://upload.wikimedia.org/wikipedia/commons/1/13/Tunnel_View%2C_Yosemite_Valley%2C_Yosemite_NP_-_Diliff.jpg`}
-              />
-            </ImageListItem>
-
-            <ImageListItem>
-              <Image
-                src={`https://upload.wikimedia.org/wikipedia/commons/1/13/Tunnel_View%2C_Yosemite_Valley%2C_Yosemite_NP_-_Diliff.jpg`}
-              />
-            </ImageListItem>
-
-            <ImageListItem>
-              <Image
-                src={`https://upload.wikimedia.org/wikipedia/commons/1/13/Tunnel_View%2C_Yosemite_Valley%2C_Yosemite_NP_-_Diliff.jpg`}
-              />
-            </ImageListItem>
+          <ImageList cols={3} gap={16} rowHeight={256} sx={{ height: "100%" }}>
+            {imageURLs.map((imageURL: string) => {
+              return (
+                <ImageListItem
+                  onClick={() => this.openModal(imageURL)}
+                  key={uuidv4()}
+                >
+                  <img
+                    src={imageURL}
+                    style={{ objectFit: "cover" }}
+                    height={256}
+                  />
+                </ImageListItem>
+              );
+            })}
           </ImageList>
+
+          {/* Modal */}
+          <Modal open={isModalOpen} onClose={this.closeModal}>
+            <Box
+              sx={{
+                position: "absolute" as "absolute",
+                top: "50%",
+                left: "50%",
+                transform: "translate(-50%, -50%)",
+                width: "70%",
+                bgcolor: "background.paper",
+                border: "2px solid #000",
+                boxShadow: 24,
+                p: 4,
+              }}
+            >
+              <img width="100%" src={selectedImage} />
+            </Box>
+          </Modal>
         </Container>
       </div>
     );
+  }
+
+  openModal(imageURL: string) {
+    this.setState({
+      isModalOpen: true,
+      selectedImage: imageURL,
+    });
+  }
+
+  closeModal() {
+    this.setState({
+      isModalOpen: false,
+    });
   }
 }
