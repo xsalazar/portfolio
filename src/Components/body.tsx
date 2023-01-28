25dcdd199a6cce2f915fd48ef3a157a0e667a390
Xavier Salazar <x.salazar1729@gmail.com>
2023-01-27 20:43:27 -0800

    Minor layout tweaks

--- src/Components/body.tsx
+++ src/Components/body.tsx
@@ -51,7 +51,7 @@ export default class Body extends React.Component<
     return (
       <div style={{ height: "calc(100vh - 200px)" }}>
         <Container
-          maxWidth="md"
+          maxWidth="sm"
           sx={{
             alignItems: "center",
             display: "flex",
@@ -89,7 +89,7 @@ export default class Body extends React.Component<
           <Modal open={isModalOpen} onClose={this.closeModal}>
             <Box
               sx={{
-                position: "absolute" as "absolute",
+                position: "absolute",
                 top: "50%",
                 left: "50%",
                 transform: "translate(-50%, -50%)",
